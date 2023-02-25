afc8d1af412b380cd611f01b33fd8d32d26df5e6
Douglinhas <daguiaralcantara@gmail.com>
2023-02-24 21:03:12 -0300

    git commit -m "test: ensure LoginController returns 400 if no password is provided"

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -15,4 +15,17 @@ describe("Login Controller", () => {
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
   })
+
+  it("Should return 400 if no password is provided", async () => {
+    const sut = new LoginController();
+    const httpRequest = {
+      body: {
+        email: "valid@mail.com"
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  })
 })
